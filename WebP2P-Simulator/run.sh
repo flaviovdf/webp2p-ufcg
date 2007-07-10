57c98b02dfae675446cf81487a2ba321cfe9cafe
flavio.barata <flavio.barata@2723ca45-a02a-0410-a9e2-6df9d64bde64>
2007-07-10 20:08:57 +0000

    initial java heap size changed to 512MB maximum java heap size changed to 1024MB

--- WebP2P-Simulator/run.sh
+++ WebP2P-Simulator/run.sh
@@ -1,1 +1,1 @@
-java -cp classes:lib/distributions.jar:lib/log4j-1.2.14.jar webp2p_sim.core.Main $*
+java -Xms512m -Xmx1024m -cp classes:lib/distributions.jar:lib/log4j-1.2.14.jar webp2p_sim.core.Main $*
